f9cad7436f4c79d8cb640f3d7006494a9f9a13d2
sourav-bluup <sourav@bluup.in>
2026-07-09 14:23:44 +0530

    registered display setting widget:

--- src/admin/widgets/index.ts
+++ src/admin/widgets/index.ts
@@ -1,3 +1,4 @@
+import ProductDiscountWidget from './product-discount';
 import ReservationStageWidget from './reservation-stage-widget';
 
-export const widgets = [ReservationStageWidget];
+export const widgets = [ReservationStageWidget, ProductDiscountWidget];
